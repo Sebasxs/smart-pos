bde019c5677b240cc5f787b4a1a06ad885acaa64
Sebasxs <sebasxs@gmail.com>
2025-09-08 14:05:02 -0500

    chore(infra): setup docker and env configuration for cloud deployment

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,3 +1,4 @@
+import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { loadEnvFile } from 'node:process';
 
@@ -5,9 +6,11 @@ const environment: string = process.env.NODE_ENV || 'development';
 const envFileName: string = environment === 'production' ? '.env.production' : '.env.development';
 const specificEnvPath = resolve(process.cwd(), envFileName);
 
-try {
-   console.log(`Cargando archivo de configuración: ${specificEnvPath}`);
-   loadEnvFile(specificEnvPath);
-} catch (error) {
-   console.error(`Error al cargar el archivo ${envFileName}:`, error);
+if (existsSync(specificEnvPath)) {
+   try {
+      console.log(`Cargando archivo de configuración local: ${specificEnvPath}`);
+      loadEnvFile(specificEnvPath);
+   } catch (error) {
+      console.error(`Error al leer el archivo ${envFileName}, pero continuando...`, error);
+   }
 }
