2e8181f14cbe37e89d407effca19d3ca5b15cb7c
Sebasxs <sebasxs@gmail.com>
2025-09-12 14:05:02 -0500

    fix(server): add NODE_OPTIONS with --no-warnings

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,16 +1,13 @@
-import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { loadEnvFile } from 'node:process';
 
 const environment: string = process.env.NODE_ENV || 'development';
 const envFileName: string = environment === 'production' ? '.env.production' : '.env.development';
-const specificEnvPath = resolve(process.cwd(), envFileName);
 
-if (existsSync(specificEnvPath)) {
-   try {
-      console.log(`Cargando archivo de configuración local: ${specificEnvPath}`);
-      loadEnvFile(specificEnvPath);
-   } catch (error) {
-      console.error(`Error al leer el archivo ${envFileName}, pero continuando...`, error);
-   }
+try {
+   const specificEnvPath = resolve(process.cwd(), envFileName);
+   console.log(`Cargando archivo de configuración local: ${specificEnvPath}`);
+   loadEnvFile(specificEnvPath);
+} catch (error) {
+   console.error(`Error al leer el archivo ${envFileName}, pero continuando...`, error);
 }
